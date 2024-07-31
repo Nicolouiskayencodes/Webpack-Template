a89218535a202b0ca9b484ff7bde55409b6a6363
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-07-30 20:34:43 -0700

    Set up webpack template

--- webpack.config.js
+++ webpack.config.js
@@ -35,10 +35,6 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
     ],
   },
 };
